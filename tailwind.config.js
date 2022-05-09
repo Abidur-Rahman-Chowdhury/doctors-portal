ba46e78df8a1472e34479eb4f641aef91c785001
Abidur Rahman Chowdhury <abid43iiuc@gmail.com>
2022-05-10 00:00:45 +0600

    done navbar

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,21 +1,19 @@
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
+  content: ['./src/**/*.{js,jsx,ts,tsx}'],
   daisyui: {
     themes: [
       {
         mytheme: {
-          primary: "#0FCFEC",
-          secondary: "#19D3AE",
-          accent: "#3A4256",
-          neutral: "#3d4451",
-          "base-100": "#ffffff",
+          primary: '#0FCFEC',
+          secondary: '#19D3AE',
+          accent: '#3A4256',
+          neutral: '#3d4451',
+          'base-100': '#ffffff',
         },
       },
-      "dark",
-      "cupcake",
+      'dark',
+      'cupcake',
     ],
   },
-  plugins: [require("daisyui")],
-}
+  plugins: [require('daisyui')],
+};
