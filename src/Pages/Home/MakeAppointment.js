544149d3bbcf71b818f495f1837d353fd3691ec4
Abidur Rahman Chowdhury <abid43iiuc@gmail.com>
2022-05-10 15:33:10 +0600

    done appointment and service section

---
+++ src/Pages/Home/MakeAppointment.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import doctor from '../../assets/images/doctor.png';
+import appointment from '../../assets/images/appointment.png';
+import PrimaryButton from '../Shared/PrimaryButton';
+
+const MakeAppointment = () => {
+    return (
+        <section style={{
+            background: `url(${appointment})`
+        }} className='flex justify-center items-center mt-40'>
+            <div className='flex-1 hidden lg:block'>
+                <img className='lg:mt-[-200px]' src={doctor} alt="" />
+            </div>
+            <div className='flex-1'>
+                <h3 className='text-xl text-primary font-bold'>Appointment</h3>
+                <h2 className='text-3xl text-white'>Make an Appointment Today</h2>
+                <p className='text-white'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Non quis, error aliquid blanditiis ratione architecto, quasi itaque aliquam totam optio labore unde recusandae aspernatur magnam rem vitae autem libero corrupti a omnis eaque excepturi. Quasi atque consequuntur voluptatibus provident non?</p>
+                <PrimaryButton >Make Appointment</PrimaryButton>
+            </div>
+        </section>
+    );
+};
+
+export default MakeAppointment;
--- src/Pages/Home/Services.js
+++ src/Pages/Home/Services.js
@@ -1,8 +1,54 @@
 import React from 'react';
+import Service from './Service';
+import fluoride from '../../assets/images/fluoride.png';
+import whitening from '../../assets/images/whitening.png';
+import cavity from '../../assets/images/cavity.png';
+
+
 
 const Services = () => {
+    const services = [
+
+        {
+            _id: 1,
+            name: 'Fluoride Treatment',
+            des: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eos, rerum.',
+            img: fluoride,
+            
+
+        },
+
+        {
+            _id: 2,
+            name: 'Cavity Filling',
+            des: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eos, rerum.',
+            img: cavity,
+            
+
+        },
+
+        {
+            _id: 3,
+            name: 'Teeth Whitening',
+            des: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eos, rerum.',
+            img: whitening,
+            
+
+        },
+    ]
     return (
-        
+        <div className='mt-20'>
+            <div className='text-center '>
+            <h3 className='text-primary text-xl font-bold uppercase'>Our Services</h3>
+            <h2 className='text-4xl '>Services We Provide</h2>
+            
+            </div>
+            <div className='grid gap-10  grid-cols-1 md:grid-cols-2 lg:grid-cols-3 mt-10'>
+                {
+                    services.map (service => <Service key={service._id} service={service}></Service>)
+                }
+           </div>
+        </div>
     );
 };
 
